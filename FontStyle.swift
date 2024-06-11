cae232bd29c93833ef8c84eb9ee35d1ae0e56788
miavo090821 <122918592+miavo090821@users.noreply.github.com>
2024-06-11 14:00:10 +0100

    Add files via upload

---
+++ FontStyle.swift
@@ -0,0 +1,19 @@
+//
+//  FontStyle.swift
+//  nailScope
+//
+//  Created by Iriss Vivi on 08/01/2024.
+
+import Foundation
+import SwiftUI
+
+extension Font{
+    static var navTitle: Font {
+        return Font.custom("Inter-SemiBold",size: 17)
+    
+    }
+    static var tastText: Font{
+        return Font.custom("Inter-Light",size:14)
+    }
+}
+
